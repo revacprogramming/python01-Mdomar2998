2c2722b5fa263d47854add28911a962762c75955
mdomar2998 <95675143+Mdomar2998@users.noreply.github.com>
2022-07-04 04:53:58 +0000

    Done

--- ActivitySet01/problem09.py
+++ ActivitySet01/problem09.py
@@ -1,3 +1,14 @@
 # Lists
 
-filename = "dataset/romeo.txt"
+file=input('enter file name: ') 
+ fh=open(file) 
+ t=[] 
+ for line in fh: 
+     str1=line.rstrip().split() 
+     for ele in str1: 
+         if ele not in t: 
+             t.append(ele) 
+         else: 
+             continue 
+ t.sort() 
+ print(t)
